3b5c30d1b82b742917a28aa880ce228afaa70af9
rionkhan-codes <rion.codes@gmail.com>
2025-04-19 23:23:38 +0600

    footer part

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ $('.counter').counterUp({
     delay: 50,
     time: 3000
 });
+// ---------- counter plgin part end ----------
+// ------------ slick slider part start -----------
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+// ------------ slick slider part end -----------
