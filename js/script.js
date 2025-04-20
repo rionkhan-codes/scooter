24a62d2921ed3dda1adbcdb6614439e750408c89
rionkhan-codes <rion.codes@gmail.com>
2025-04-20 23:51:02 +0600

    slider part left

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ $('.counter').counterUp({
     time: 3000
 });
 // ---------- counter plgin part end ----------
+// ---------- slick slider start -----------
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    arrows:false,
+  });
+// ---------------- slick slider part 2 ---------
+$('.reviewPart').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows:true
+});
+
